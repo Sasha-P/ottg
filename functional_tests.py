9f44cf6a2d9b446651a61003143f9916fe196507
Sasha-P <sasha.pazyuk@gmail.com>
2017-01-06 17:57:38 +0200

    First FT specced out in comments, and now uses unittest.

--- functional_tests.py
+++ functional_tests.py
@@ -1,27 +1,66 @@
 import os
+import unittest
+
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 # from selenium.webdriver.common.keys import Keys
 
 Chrome, Firefox, Opera = range(3)
 WEB_DRIVERS = {
-    'dir': 'drivers',
+    'dir': '../drivers',
     Chrome: 'chromedriver',
     Firefox: 'geckodriver',
     Opera: 'operadriver'
 }
 
-browser_name = Firefox
-base_dir = os.path.dirname(os.path.abspath(__file__))
-complete_path = os.path.join(base_dir, WEB_DRIVERS['dir'], WEB_DRIVERS[browser_name])
 
-caps = DesiredCapabilities.FIREFOX
-caps["marionette"] = True
-caps["binary"] = "/usr/bin/firefox"
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        browser_name = Firefox
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        complete_path = os.path.join(base_dir, WEB_DRIVERS['dir'], WEB_DRIVERS[browser_name])
 
-browser = webdriver.Firefox(capabilities=caps, executable_path=complete_path)
-browser.implicitly_wait(10)
-try:
-    browser.get('http://localhost:8000')
-finally:
-    assert 'Django' in browser.title
+        caps = DesiredCapabilities.FIREFOX
+        caps["marionette"] = True
+        caps["binary"] = "/usr/bin/firefox"
+
+        self.browser = webdriver.Firefox(capabilities=caps, executable_path=complete_path)
+        self.browser.implicitly_wait(10)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list
+
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+
+        # The page updates again, and now shows both items on her list
+
+        # Edith wonders whether the site will remember her list. Then she sees
+        # that the site has generated a unique URL for her -- there is some
+        # explanatory text to that effect.
+
+        # She visits that URL - her to-do list is still there.
+
+        # Satisfied, she goes back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
